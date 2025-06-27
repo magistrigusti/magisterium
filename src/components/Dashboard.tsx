c60380ed30bb731342a9d09f5f5795ec4cb94584
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-06-27 16:54:11 +0300

    0.1.1

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+
+type Props = {};
+
+function Dashboard({}: Props) {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = () => {
             href="/"
           >
             <Logo />
-            <p className="font-bold text-inherit">Magisterium</p>
+            <p className="font-bold text-inherit">Mercatus</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
